2873035bce74793da0d5821d63dba79f1630e644
Zach Furman <zach.furman1@gmail.com>
2016-05-25 23:11:28 -0700

    Added character

--- main.js
+++ main.js
@@ -5,4 +5,24 @@ window.onload = function() {
 	document.getElementById('container').appendChild(renderer.view);
 	stage = new PIXI.Stage();
 
+	var graphics = new PIXI.Graphics();
+
+	// set a fill and line style
+	graphics.beginFill(0xffffff);
+	graphics.lineStyle(10, 0xffffff, 0);
+	
+	// draw a shape
+	graphics.moveTo(100, 100);
+	graphics.lineTo(130, 180);
+	graphics.lineTo(100, 170);
+	graphics.lineTo(70, 180);
+	graphics.endFill();
+
+	stage.addChild(graphics);
+
+	requestAnimationFrame(animate);
+
+	function animate() {
+		renderer.render(stage);
+	}
 }
